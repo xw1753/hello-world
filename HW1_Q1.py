8d211005d60dac8b00f2148bed0ef9e5c0d4918a
xw1753 <113062555+xw1753@users.noreply.github.com>
2022-09-12 17:19:17 -0400

    Add files via upload

---
+++ HW1_Q1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+
+root = '/Users/wxq/MLSys-NYU-2022/weeks/2/data/'
+data = pd.read_csv(root+'train.csv')
+data.head()
+
+X = data[['1stFlrSF','2ndFlrSF','TotalBsmtSF']].values
+y = data[['SalePrice']].values
+
+a = np.linalg.inv(np.transpose(X) @ X)
+beta = (a @ np.transpose(X))@y
+beta
+
+y_pred = X.dot(beta)
+y_pred
+
+y_bar = y.mean()
+y_bar
+
+r_squared = 1 - ((y - y_pred)**2).sum() / ((y - y_bar)**2).sum() 
+print(r_squared)
+
+
